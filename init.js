e7242718e49f0ba4c6a54e503d85e15900314a99
Ricky Romero <ricky.romero@gmail.com>
2022-09-13 10:36:00 -0700

    Separate setup from init for Firefox compat

--- init.js
+++ init.js
@@ -6,32 +6,6 @@ importScripts(
   "scripts/private/blocker.js",
   "scripts/private/ui-bridge.js",
   "scripts/private/task-queue.js",
-  "scripts/private/upgrade.js"
+  "scripts/private/upgrade.js",
+  "scripts/private/setup.js"
 )
-
-chrome.runtime.onInstalled.addListener(() => {
-  // Disable action on all tabs... at first.
-  chrome.action.disable()
-})
-
-/* eslint-disable no-unused-vars */
-const allowlist = new Allowlist()
-const blocker = new Blocker()
-const options = new Options()
-const uiBridge = new UIBridge()
-uiBridge.addListeners()
-/* eslint-enable no-unused-vars */
-
-;(async () => {
-  await Storage.queueOperation((async function () {
-    await uiBridge.addContextMenu(options)
-    await runUpgrade()
-  }).bind(this))
-
-  await Storage.queueOperation(() => {
-    // Init context menus
-    chrome.contextMenus.onClicked.addListener((_, tab) => {
-      uiBridge.toggleBlockerStates(tab)
-    })
-  })
-})()
---
+++ scripts/private/setup.js
@@ -0,0 +1,26 @@
+chrome.runtime.onInstalled.addListener(() => {
+  // Disable action on all tabs... at first.
+  chrome.action.disable()
+})
+
+/* eslint-disable no-unused-vars */
+const allowlist = new Allowlist()
+const blocker = new Blocker()
+const options = new Options()
+const uiBridge = new UIBridge()
+uiBridge.addListeners()
+/* eslint-enable no-unused-vars */
+
+;(async () => {
+  await Storage.queueOperation((async function () {
+    await uiBridge.addContextMenu(options)
+    await runUpgrade()
+  }).bind(this))
+
+  await Storage.queueOperation(() => {
+    // Init context menus
+    chrome.contextMenus.onClicked.addListener((_, tab) => {
+      uiBridge.toggleBlockerStates(tab)
+    })
+  })
+})()
